35afd747019822acf8b9ee51edd1d61300fa781a
thanhnhile <marrythanhnhipl205@gmail.com>
2022-05-17 22:07:23 +0700

    booking table

---
+++ restaurant/src/main/java/com/hcmute/dao/CustomerDao.java
@@ -0,0 +1,48 @@
+package com.hcmute.dao;
+
+import com.hcmute.model.CustomerModel;
+import com.hcmute.utils.DdUtil;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class CustomerDao {
+    Connection connection = null;
+    PreparedStatement ps = null;
+    ResultSet rs = null;
+    public int insert(CustomerModel customer){
+        int id = 0;
+        try{
+            String sql = "insert into customer(name,address,phone,img) value(?,?,?,?)";
+            connection = DdUtil.getConnection();
+            ps = connection.prepareStatement(sql);
+            ps.setString(1,customer.getName());
+            ps.setString(2,customer.getAddress());
+            ps.setString(3,customer.getPhone());
+            ps.setString(4,customer.getImg());
+            if(ps.executeUpdate() > 0){
+                id = getNewestID();
+            }
+
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        return id;
+    }
+    public int getNewestID(){
+        try{
+            String sql = "select id from customer order by id desc";
+            connection = DdUtil.getConnection();
+            ps = connection.prepareStatement(sql);
+            rs = ps.executeQuery();
+            while (rs.next()){
+                return rs.getInt(1);
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return 0;
+    }
+}
