f1314e302f50404b73f0b9b055219d1a3cfd25d7
cuadong <thanhphuoc01012001@gmail.com>
2022-05-21 01:08:28 +0700

    add + edit + delete cart

--- restaurant/src/main/java/com/hcmute/controller/user/CartController.java
+++ restaurant/src/main/java/com/hcmute/controller/user/CartController.java
@@ -1,6 +1,7 @@
 package com.hcmute.controller.user;
 
 import com.hcmute.dao.CartDao;
+import com.hcmute.dao.CustomerDao;
 import com.hcmute.dao.ProductDao;
 import com.hcmute.model.AccountModel;
 import com.hcmute.model.CartModel;
@@ -18,6 +19,7 @@ import java.util.List;
 public class CartController {
     public CartDao cartDao = new CartDao();
     public ProductDao productDao = new ProductDao();
+    public CustomerDao customerDao = new CustomerDao();
 
     private int existProduct(int product_id, List<CartModel> cart) {
         for (int i = 0; i < cart.size(); i++) {
@@ -32,43 +34,76 @@ public class CartController {
     @RequestMapping("/add/{product_id}")
     public ModelAndView Add(HttpSession session, @PathVariable int product_id) {
         ModelAndView mav = new ModelAndView("redirect:/view-product/" + product_id);
-        AccountModel account = (AccountModel) session.getAttribute("account"); // TODO thiếu customer id
+        AccountModel account = (AccountModel) session.getAttribute("account");
+        int customer_id = account.getCustomer_id();
         ProductModel product = productDao.getProductById(product_id);
         CartModel itemCart = new CartModel();
         itemCart.setProductId(product.getId());
         itemCart.setProductAmount(1);
-        itemCart.setCusId(3); //TODO thiếu customer id
+        itemCart.setCusId(customer_id);
         itemCart.setImg(product.getImg());
         itemCart.setProduct_name(product.getName());
         itemCart.setProduct_price(product.getPrice());
         itemCart.setState(0);
-        if (cartDao.existCart(3)) //TODO thiếu customer id
+        if (cartDao.existCart(customer_id))
         {
-            List<CartModel> cart = cartDao.selectCart(3); // TODO thiếu customer id
+            List<CartModel> cart = cartDao.selectCart(customer_id);
             int i = existProduct(product_id, cart);
             if (i != -1) {
                 cart.get(i).setProductAmount(cart.get(i).getProductAmount() + 1);
                 cartDao.updateCart(cart.get(i));
             } else {
-                int cart_id = cartDao.getIdCart(3);//TODO thiếu customer id
+                int cart_id = cartDao.getIdCart(customer_id);
                 itemCart.setId(cart_id);
                 cartDao.insertCart(itemCart);
             }
         }
         else{
             cartDao.createCart(itemCart);
-            int cart_id = cartDao.getIdCart(3);//TODO thiếu customer id
+            int cart_id = cartDao.getIdCart(customer_id);
             itemCart.setId(cart_id);
             cartDao.insertCart(itemCart);
         }
+        int totalAmount = cartDao.totalAmount(customer_id);
+        session.setAttribute("totalAmount", totalAmount);
         return mav;
     }
 
     @RequestMapping("/view/{customer_id}")
-    public ModelAndView Index(@PathVariable int customer_id) {
+    public ModelAndView Index(HttpSession session, @PathVariable int customer_id) {
         ModelAndView mav = new ModelAndView("cart");
         mav.addObject("cart", cartDao.selectCart(customer_id));
+        session.setAttribute("totalPrice", cartDao.totalPrice(customer_id));
+        return mav;
+    }
+    @RequestMapping("/edit/{product_id}/{product_amount}")
+    public ModelAndView Edit(HttpSession session, @PathVariable int product_id, @PathVariable int product_amount) {
+        AccountModel account = (AccountModel) session.getAttribute("account");
+        int customer_id = account.getCustomer_id();
+        ModelAndView mav = new ModelAndView("redirect:/cart/view/" + customer_id);
+        CartModel itemCart = new CartModel();
+        itemCart.setId(cartDao.getIdCart(customer_id));
+        itemCart.setProductId(product_id);
+        itemCart.setProductAmount(product_amount);
+        cartDao.updateCart(itemCart);
+
+        session.setAttribute("totalAmount", cartDao.totalAmount(customer_id));
+        session.setAttribute("totalPrice", cartDao.totalPrice(customer_id));
         return mav;
     }
 
+    @RequestMapping("/delete/{product_id}")
+    public ModelAndView Delete(HttpSession session, @PathVariable int product_id) {
+        AccountModel account = (AccountModel) session.getAttribute("account");
+        int customer_id = account.getCustomer_id();
+        ModelAndView mav = new ModelAndView("redirect:/cart/view/" + customer_id);
+        int cart_id = cartDao.getIdCart(customer_id);
+        cartDao.deleteCart(cart_id, product_id);
+
+        session.setAttribute("totalAmount", cartDao.totalAmount(customer_id));
+
+        session.setAttribute("totalPrice", cartDao.totalPrice(customer_id));
+
+        return mav;
+    }
 }
